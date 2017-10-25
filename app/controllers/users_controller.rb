fae7ea9fa856c9ae64d8b590a889af8924814744
kishorecherukuror <kishorecheruku.ror@gmail.com>
2017-10-25 14:33:47 +0530

    Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,30 @@ class UsersController < ApplicationController
   def create
   	#byebug
   	@fullname = params[:fname] + params[:lname]
-
-  	@age = Time.now.strftime('%Y').to_i - params[:dob].split("-").first.to_i
+    @age = calculate_age(params[:dob])
   	@hobbies = params[:rock].join(",") if params[:rock]
   	@gender = params[:gender]
   	@qualification = params[:qualification]
+  end
+
+  def calculate_age(dob)
+    def humanize secs
+      [ 
+        [60, :seconds], 
+        [60, :minutes], 
+        [24, :hours], 
+        [365, :days], 
+        [100, :years]
+      ].map do |count, name|
+        #byebug
+        if secs > 0
+          secs, n = secs.divmod(count)
+          "#{n.to_i} #{name}"
+        end
+      end.compact.reverse.join(' ')
+    end
+    distance = Time.new - Time.parse(dob)
+    humanize(distance).split(" ").take(4).join(",")
   end
 
   def show
